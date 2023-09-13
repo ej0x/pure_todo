e2880df23598690b80284daad7853911fc85e291
ej0x <ejas01@outlook.com>
2023-09-13 19:52:02 +0530

    assets and HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pure_todo/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: HomePage(),
     );
   }
 }
